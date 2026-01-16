e8b0e22ae7d2708c2cf3b031f12de2e9dfd39f38
surajsanadi <surajsanadi008@gmail.com>
2026-01-17 00:02:38 +0530

    Update ssh.tf

--- terraform-VM-creation/ssh.tf
+++ terraform-VM-creation/ssh.tf
@@ -3,6 +3,7 @@ resource "azurerm_ssh_public_key" "ssh-ss" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   #public_key          = file("~/.ssh/id_rsa.pub")
-  public_key          = file("C:/Users/Suri/.ssh/id_rsa.pub")
+  public_key          = file("C:/Users/Suri/.ssh/id_ed25519.pub")
 }
+
 
